440eb7e94947fb6e5bda4b61b32e7248766a2d8e
galiphasandugan <galiphasandugan@gmail.com>
2023-08-26 21:45:18 +0200

    changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-function App() {
+export default function App() {
   return (
-    <div className="bg-green-400 h-screen p-[50px] ">
-      <h1>Tailwind Css Kurulumu</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="flex gap-3 mt-4">
-         <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
-         <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
+    <div className="bg-fuchsia-500 h-screen p-[10px]">
+      <h1 className="text-8xl ">Tailwind css lurulum</h1>
+      <h1 className="text-4xl bg-[#ededed] font-bold text-center  underline">
+        Hello world!
+      </h1>
+      <div className="flex flex-col sm:flex-row gap-3 mt-4">
+        <div className="card">
+          <img     src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+            alt="sa" className="rounded-xl h-[200px] " />
+            <p className="line-clamp-1 hover:line-clamp-none ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quos ullam fuga, vitae expedita dignissimos dolorum accusamus blanditiis repellat, ipsa explicabo sint consequatur autem eaque perspiciatis quibusdam natus beatae deleniti. Expedita ipsam dignissimos consequatur perspiciatis?</p>
+        </div>
+
+        <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 "></div>
+
       </div>
-     
     </div>
   );
 }
-
-export default App;
-    
