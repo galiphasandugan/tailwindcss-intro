0e5596979eeadc649848cf85cc88c8d25d77f561
galiphasandugan <galiphasandugan@gmail.com>
2023-08-26 19:55:30 +0200

    changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 function App() {
   return (
-    <div>
+    <div className="bg-green-400 h-screen p-[50px] ">
       <h1>Tailwind Css Kurulumu</h1>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex gap-3 mt-4">
+         <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
+         <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
+      </div>
+     
     </div>
   );
 }
 
 export default App;
+    
